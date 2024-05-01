c4977995a74137cabba6cf591595231e07b90ced
HAIOVIY <gaevoywork@gmail.com>
2024-05-01 21:48:04 +0300

    added exception handler and validation for RequestDto

--- src/main/java/mate/academy/mybookshop/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/mybookshop/dto/CreateBookRequestDto.java
@@ -1,13 +1,24 @@
 package mate.academy.mybookshop.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
+    @NotBlank
+    @NotNull
     private String title;
+    @NotBlank
+    @NotNull
     private String author;
+    @Pattern(regexp = "\\d{13}", message = "must contain exactly 13 digits")
     private String isbn;
+    @Min(value = 0)
+    @NotNull
     private BigDecimal price;
     private String description;
     private String coverImage;
