f64777a6bc46ea6570f9d4f1826b0b02fec50665
HAIOVIY <gaevoywork@gmail.com>
2024-05-09 16:39:48 +0300

    removed unused method

--- src/main/java/mate/academy/mybookshop/annotation/PasswordMatchesValidator.java
+++ src/main/java/mate/academy/mybookshop/annotation/PasswordMatchesValidator.java
@@ -8,11 +8,6 @@ import mate.academy.mybookshop.dto.UserRegistrationRequestDto;
 public class PasswordMatchesValidator implements ConstraintValidator<PasswordMatches, Object> {
 
     @Override
-    public void initialize(PasswordMatches constraintAnnotation) {
-        //ignored
-    }
-
-    @Override
     public boolean isValid(Object value, ConstraintValidatorContext context) {
         if (value instanceof UserRegistrationRequestDto userDto) {
             return Objects.equals(userDto.getPassword(), userDto.getConfirmPassword());
--- src/main/java/mate/academy/mybookshop/service/UserService.java
+++ src/main/java/mate/academy/mybookshop/service/UserService.java
@@ -2,10 +2,9 @@ package mate.academy.mybookshop.service;
 
 import mate.academy.mybookshop.dto.UserRegistrationRequestDto;
 import mate.academy.mybookshop.dto.UserResponseDto;
-import mate.academy.mybookshop.exception.RegistrationException;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface UserService {
-    UserResponseDto registerUser(UserRegistrationRequestDto request) throws RegistrationException;
+    UserResponseDto registerUser(UserRegistrationRequestDto request);
 }
