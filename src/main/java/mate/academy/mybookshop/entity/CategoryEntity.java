8e7dee723e3cde9d78fea6be091c698336bda153
HAIOVIY <gaevoywork@gmail.com>
2024-05-16 18:35:04 +0300

    added CategoryEntity

---
+++ src/main/java/mate/academy/mybookshop/entity/CategoryEntity.java
@@ -0,0 +1,27 @@
+package mate.academy.mybookshop.entity;
+
+import lombok.Data;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Data
+@SQLDelete(sql = "UPDATE categories SET is_deleted = true WHERE id=?")
+@SQLRestriction(value = "is_deleted=false")
+@Table(name = "categories")
+public class CategoryEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    private String description;
+    @Column(nullable = false)
+    private boolean isDeleted = false;
+}
